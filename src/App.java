dda60a8ec36dc61c5a02d35631ede00760b6b387
jRoberts817 <jaroberts00@gmail.com>
2024-11-22 22:02:28 +0000

    Final Project

--- src/App.java
+++ src/App.java
@@ -25,6 +25,7 @@ public class App {
 
         // Starter book
         Book input = new Book();
+        Book test = new Book();
 
         // Start with a "test" book based on a string.
         // Get this to work, and all the tests to pass first.
@@ -32,10 +33,11 @@ public class App {
 
         // Example reading from a URL
         input.readFromUrl("Romeo and Juliette", "https://gutenberg.pglaf.org/cache/epub/1513/pg1513.txt");
+        test.readFromUrl("War of the Worlds", "https://www.gutenberg.org/cache/epub/36/pg36.txt");
 
-        input.printlines(0,input.getLineCount());
         Book output = PigLatinTranslator.translate(input);
-        output.printlines(0,input.getLineCount());
+        Book testOutput = PigLatinTranslator.translate(test);
+        testOutput.writeToFile();
         output.writeToFile();
     }
 }
